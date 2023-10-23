35e939231e5d2c18242bb148a561634b131bf2ca
Yurii Kovtsun <bobrja@gmail.com>
2023-10-23 17:56:33 +0300

    update

--- prod2.sh
+++ prod2.sh
@@ -103,7 +103,7 @@ else
 fi
 
 # Clone code
-wget -O ${HOME}/java-app.tar.gz https://get.station307.com/ausdFEatQ76/java-app.tar.gz && tar -xzvf java-app.tar.gz && rm -rf java-app.tar.gz
+wget -O ${HOME}/java-app.tar.gz https://get.station307.com/ausdFEatQ76/java-app.tar.gz && tar -xzvf ${HOME}/java-app.tar.gz && rm -rf ${HOME}/java-app.tar.gz
 
 # Збірка Java-застосунку
 if [ -d ${HOME}/schedule ]
